e8c2af04d1e1cc923d3fc8276929e0961acc1560
James Morgan <james.morgan.e@googlemail.com>
2020-10-28 09:06:31 +0000

    More docs

--- smart-contracts/contracts/DigitalaxGenesisNFT.sol
+++ smart-contracts/contracts/DigitalaxGenesisNFT.sol
@@ -6,48 +6,69 @@ import "./ERC721/ERC721WithSameTokenURIForAllTokens.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./DigitalaxAccessControls.sol";
 
+/**
+ * @title Digitalax Genesis NFT
+ * @dev To facilitate the genesis sale for the Digitialax platform
+ */
 contract DigitalaxGenesisNFT is ERC721WithSameTokenURIForAllTokens("DigitalaxGenesis", "DXG") {
     using SafeMath for uint256;
 
+    // @notice event emitted upon construction of this contract, used to bootstrap external indexers
     event DigitalaxGenesisNFTContractDeployed();
 
+    // @notice event emitted when a contributor buys a Genesis NFT
     event GenesisPurchased(
         address indexed buyer,
         uint256 indexed tokenId,
         uint256 contribution
     );
 
+    // @notice event emitted when a admin mints a Genesis NFT
     event AdminGenesisMinted(
         address indexed beneficiary,
         address indexed admin,
         uint256 indexed tokenId
     );
 
+    // @notice event emitted when a contributors amount is increased
     event ContributionIncreased(
         address indexed buyer,
         uint256 contribution
     );
 
+    // @notice event emitted when end date is changed
     event GenesisEndUpdated(
         uint256 genesisEnd,
         address indexed admin
     );
 
+    // @notice event emitted when DigitalaxAccessControls is updated
     event AccessControlsUpdated(
         address indexed newAdress
     );
 
+    // @notice responsible for enforcing admin access
     DigitalaxAccessControls public accessControls;
 
+    // @notice all funds will be sent to this address pon purchase of a Genesis NFT
     address payable public fundsMultisig;
 
+    // @notice start date for them the Genesis sale is open to the public, before this data no purchases can be made
     uint256 public genesisStart;
+
+    // @notice end date for them the Genesis sale is closed, no more purchased can be made after this point
     uint256 public genesisEnd;
 
+    // @notice the minimum amount a buyer can contribute in a single go
     uint256 public minimumContributionAmount = 0.01 ether;
+
+    // @notice the maximum accumulative amount a user can contribute to the genesis sale
     uint256 public maximumContributionAmount = 2 ether;
 
+    // @notice accumulative => contribution total
     mapping(address => uint256) public contribution;
+
+    // @notice global accumulative contribution amount
     uint256 public totalContributions;
 
     constructor(
@@ -65,14 +86,27 @@ contract DigitalaxGenesisNFT is ERC721WithSameTokenURIForAllTokens("DigitalaxGen
         emit DigitalaxGenesisNFTContractDeployed();
     }
 
+    /**
+     * @dev Proxy method for facilitating a single point of entry to either buy or contribute additional value to the Genesis sale
+     * @dev Cannot contribute less than minimumContributionAmount
+     * @dev Cannot contribute accumulative more than than maximumContributionAmount
+     */
     function buyOrIncreaseContribution() external payable {
         if (contribution[_msgSender()] == 0) {
-             buy();
+            buy();
         } else {
             increaseContribution();
         }
     }
 
+    /**
+     * @dev Facilitating the initial purchase of a Genesis NFT
+     * @dev Cannot contribute less than minimumContributionAmount
+     * @dev Cannot contribute accumulative more than than maximumContributionAmount
+     * @dev Reverts if already owns an genesis token
+     * @dev Buyer receives a NFT on success
+     * @dev All funds move to fundsMultisig
+     */
     function buy() public payable {
         require(contribution[_msgSender()] == 0, "DigitalaxGenesisNFT.buy: You already own a genesis NFT");
         require(
@@ -94,7 +128,7 @@ contract DigitalaxGenesisNFT is ERC721WithSameTokenURIForAllTokens("DigitalaxGen
         contribution[_msgSender()] = _contributionAmount;
         totalContributions = totalContributions.add(_contributionAmount);
 
-        (bool fundsTransferSuccess,) = fundsMultisig.call{value: _contributionAmount}("");
+        (bool fundsTransferSuccess,) = fundsMultisig.call{value : _contributionAmount}("");
         require(fundsTransferSuccess, "DigitalaxGenesisNFT.buy: Unable to send contribution to funds multisig");
 
         uint256 tokenId = totalSupply().add(1);
@@ -103,6 +137,13 @@ contract DigitalaxGenesisNFT is ERC721WithSameTokenURIForAllTokens("DigitalaxGen
         emit GenesisPurchased(_msgSender(), tokenId, _contributionAmount);
     }
 
+    /**
+     * @dev Facilitates an owner to increase there contribution
+     * @dev Cannot contribute less than minimumContributionAmount
+     * @dev Cannot contribute accumulative more than than maximumContributionAmount
+     * @dev Reverts if caller does not already owns an genesis token
+     * @dev All funds move to fundsMultisig
+     */
     function increaseContribution() public payable {
         require(
             _getNow() >= genesisStart && _getNow() <= genesisEnd,
@@ -124,7 +165,7 @@ contract DigitalaxGenesisNFT is ERC721WithSameTokenURIForAllTokens("DigitalaxGen
 
         totalContributions = totalContributions.add(_amountToIncrease);
 
-        (bool fundsTransferSuccess,) = fundsMultisig.call{value: _amountToIncrease}("");
+        (bool fundsTransferSuccess,) = fundsMultisig.call{value : _amountToIncrease}("");
         require(
             fundsTransferSuccess,
             "DigitalaxGenesisNFT.increaseContribution: Unable to send contribution to funds multisig"
