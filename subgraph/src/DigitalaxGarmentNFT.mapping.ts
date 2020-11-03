9f1449083c3036dfab5645e43d3471e7726cb19e
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-03 16:16:10 +0000

    child parent modelling for garment nft

---
+++ subgraph/src/DigitalaxGarmentNFT.mapping.ts
@@ -0,0 +1,43 @@
+import {log, BigInt, Address} from "@graphprotocol/graph-ts/index";
+
+import {
+    Transfer,
+    ReceivedChild
+} from "../generated/DigitalaxGarmentNFT/DigitalaxGarmentNFT";
+
+import {
+    DigitalaxGarment,
+    DigitalaxGarmentChild
+} from "../generated/schema";
+
+export const ZERO_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000');
+
+export function handleTransfer(event: Transfer): void {
+    // This is the birthing of a garment
+    if (event.params.from.equals(ZERO_ADDRESS)) {
+        let garment = new DigitalaxGarment(event.params.tokenId.toString());
+        garment.save();
+    }
+}
+
+export function handleChildReceived(event: ReceivedChild): void {
+    let garment = DigitalaxGarment.load(event.params.toTokenId.toString());
+
+    let childId = event.params.toTokenId.toString() + '-' + event.params.childTokenId.toString();
+    let child = DigitalaxGarmentChild.load(childId);
+
+    if (child == null) {
+        child = new DigitalaxGarmentChild(childId);
+        child.amount = event.params.amount;
+    } else {
+        child.amount = child.amount + event.params.amount;
+    }
+
+    child.save();
+
+    let strands = garment.strands;
+    strands.push(childId);
+    garment.strands = strands;
+
+    garment.save();
+}
