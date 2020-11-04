98d9f8cf2acb4bbf84fb0e75824fe6cb527718c1
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-04 16:35:19 +0000

    materials handlers working

--- subgraph/src/DigitalaxMaterials.mapping.ts
+++ subgraph/src/DigitalaxMaterials.mapping.ts
@@ -7,29 +7,28 @@ import {
 } from "../generated/DigitalaxMaterials/DigitalaxMaterials";
 
 import {
-    DigitalaxMaterials
+    DigitalaxMaterial
 } from "../generated/schema";
 
 export function handleStrandCreated(event: StrandCreated): void {
     log.info("handleStrandCreated @ Strand ID {}", [event.params.strandId.toString()]);
     let contract = DigitalaxMaterialsContract.bind(event.address);
 
-    let strand = new DigitalaxMaterials(event.params.strandId.toString());
+    let strand = new DigitalaxMaterial(event.params.strandId.toString());
     strand.tokenUri = contract.uri(event.params.strandId);
     strand.totalSupply = BigInt.fromI32(0);
     strand.save();
 }
 
 export function handleBatchTransfer(event: TransferBatch): void {
-    log.info("handleBatchTransfer With Batch Size {}", ['1']);
+    log.info("handleBatchTransfer With Batch Size {}", [`${event.params.values.length}`]);
+    let contract = DigitalaxMaterialsContract.bind(event.address);
     let strandIds = event.params.ids;
     for(let i = 0; i < strandIds.length; i++) {
         let strandId = strandIds.pop();
 
-        let strand = DigitalaxMaterials.load(strandId.toString());
-
-        let amount = event.params.values.pop();
-        strand.totalSupply = strand.totalSupply + amount;
+        let strand = DigitalaxMaterial.load(strandId.toString());
+        strand.totalSupply = contract.strandTotalSupply(strandId);
         strand.save();
     }
 }
