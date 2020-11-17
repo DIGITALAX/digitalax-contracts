81b1d2aa62da05901eaa99e77b15809f7cd5df53
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-17 12:34:44 +0000

    rename collector fields and bug fix attempt

--- subgraph/src/DigitalaxMaterials.mapping.ts
+++ subgraph/src/DigitalaxMaterials.mapping.ts
@@ -14,9 +14,10 @@ import {
 
 import {ZERO_ADDRESS, DigitalaxGarmentNFTContractAddress, ZERO} from "./constants";
 import {loadOrCreateDigitalaxCollector} from "./factory/DigitalaxCollector.factory";
+import {isChildInList} from "./ArrayHelpers";
 
 export function handleChildCreated(event: ChildCreated): void {
-    log.info("handleChildCreated @ Child ID {}", [event.params.childId.toString()]);
+    // log.info("handleChildCreated @ Child ID {}", [event.params.childId.toString()]);
     let contract = DigitalaxMaterialsContract.bind(event.address);
 
     let strand = new DigitalaxMaterial(event.params.childId.toString());
@@ -26,7 +27,7 @@ export function handleChildCreated(event: ChildCreated): void {
 }
 
 export function handleChildrenCreated(event: ChildrenCreated): void {
-    log.info("handleChildrenCreated", []);
+    // log.info("handleChildrenCreated", []);
     let contract = DigitalaxMaterialsContract.bind(event.address);
 
     let childIds = event.params.childIds;
@@ -41,12 +42,12 @@ export function handleChildrenCreated(event: ChildrenCreated): void {
 
 // TODO add single transfer hook and handle adding/removing strands
 export function handleSingleTransfer(event: TransferSingle): void {
-    log.info("handle single transfer of child ID {} and balance {}",
-        [
-            event.params.id.toString(),
-            event.params.value.toString()
-        ]
-    );
+    // log.info("handle single transfer of child ID {} and balance {}",
+    //     [
+    //         event.params.id.toString(),
+    //         event.params.value.toString()
+    //     ]
+    // );
 
     // Ensure total supply is correct in cases of birthing or burning
     let contract: DigitalaxMaterialsContract = DigitalaxMaterialsContract.bind(event.address);
@@ -57,29 +58,28 @@ export function handleSingleTransfer(event: TransferSingle): void {
 
     // If `from` is not zero or the garment NFT, then update the collector that triggered the transfer
     if (!event.params.from.equals(ZERO_ADDRESS)
-        || !event.params.from.equals(Address.fromString(DigitalaxGarmentNFTContractAddress))) {
+        ) {
         let fromCollector = loadOrCreateDigitalaxCollector(event.params.from);
-        let strandsOwned = fromCollector.strandsOwned;
-        strandsOwned = strandsOwned.filter((childId: string) => childId !== event.params.id.toString());
-        fromCollector.strandsOwned = strandsOwned;
+        let childrenOwned = fromCollector.childrenOwned;
+        childrenOwned = childrenOwned.filter((childId: string) => childId !== event.params.id.toString());
+        fromCollector.childrenOwned = childrenOwned;
         fromCollector.save();
     }
 
     // If `to` is burn or garment NFT address, we can stop here otherwise add the strand to the `to` address
     if (!event.params.to.equals(ZERO_ADDRESS)
-        || !event.params.to.equals(Address.fromString(DigitalaxGarmentNFTContractAddress))) {
+        ) {
         let toCollector = loadOrCreateDigitalaxCollector(event.params.to);
-        let strandsOwned = toCollector.strandsOwned;
-        strandsOwned.push(strandId.toString());
-        toCollector.strandsOwned = strandsOwned;
+        let childrenOwned = toCollector.childrenOwned;
+        childrenOwned.push(strandId.toString());
+        toCollector.childrenOwned = childrenOwned;
         toCollector.save();
     }
 }
 
 export function handleBatchTransfer(event: TransferBatch): void {
-    log.info("handleBatchTransfer With Batch Size {} @ Hash {}", [
-        BigInt.fromI32(event.params.values.length).toString(),
-        event.transaction.hash.toHexString()
+    log.info("handleBatchTransfer With Batch Size {}", [
+        BigInt.fromI32(event.params.values.length).toString()
     ]);
 
     let contract: DigitalaxMaterialsContract = DigitalaxMaterialsContract.bind(event.address);
@@ -95,14 +95,19 @@ export function handleBatchTransfer(event: TransferBatch): void {
         // If `from` is not zero, then update the collector that triggered the transfer
         if (!event.params.from.equals(ZERO_ADDRESS)
             || !event.params.from.equals(Address.fromString(DigitalaxGarmentNFTContractAddress))) {
+
             let fromCollector = loadOrCreateDigitalaxCollector(event.params.from);
-            let strandsOwned = fromCollector.strandsOwned;
-            strandsOwned = strandsOwned.filter(function (childId: string) {
-                // the from collector retains any strands that are not being transferred
-                let filterResult = event.params.ids.filter((id: BigInt) => id.toString() == childId);
-                return filterResult.length == 0;
-            });
-            fromCollector.strandsOwned = strandsOwned;
+            let childrenOwned = fromCollector.childrenOwned;
+
+            let updatedChildren = new Array<string>();
+            for(let j = 0; j < childrenOwned.length; j++) {
+                let strand = childrenOwned.pop();
+                if (!isChildInList(strand.toString(), event.params.ids)) {
+                    updatedChildren.push(strand);
+                }
+            }
+
+            fromCollector.childrenOwned = updatedChildren;
             fromCollector.save();
         }
 
@@ -110,9 +115,9 @@ export function handleBatchTransfer(event: TransferBatch): void {
         if (!event.params.to.equals(ZERO_ADDRESS)
             || !event.params.to.equals(Address.fromString(DigitalaxGarmentNFTContractAddress))) {
             let toCollector = loadOrCreateDigitalaxCollector(event.params.to);
-            let strandsOwned = toCollector.strandsOwned;
-            strandsOwned.push(strandId.toString());
-            toCollector.strandsOwned = strandsOwned;
+            let childrenOwned = toCollector.childrenOwned;
+            childrenOwned.push(strandId.toString());
+            toCollector.childrenOwned = childrenOwned;
             toCollector.save();
         }
     }
