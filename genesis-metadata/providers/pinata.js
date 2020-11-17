37628be30af61fb4248b13500230722ea8a823b5
James Morgan <james.morgan.e@googlemail.com>
2020-11-17 11:12:37 +0000

    Moving minting to a new repo

--- genesis-metadata/providers/pinata.js
+++
@@ -1,8 +0,0 @@
-const pinataSDK = require('@pinata/sdk');
-const pinata = pinataSDK(process.env.PINATA_API, process.env.PINATA_SECRET);
-
-pinata
-  .testAuthentication()
-  .then((result) => console.log(`Successfully connected to pinata`, result));
-
-module.exports = pinata;
