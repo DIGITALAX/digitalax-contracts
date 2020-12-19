eb6513eda83c86b2cb0ad3c0b0bb3c01e3676b95
Chaindev <ikeda.okura@gmail.com>
2020-12-19 00:48:43 -0300

    static method of designer id mapping

--- subgraph/src/factory/DigitalaxGarmentDesigner.factory.ts
+++ subgraph/src/factory/DigitalaxGarmentDesigner.factory.ts
@@ -1,12 +1,15 @@
 import {DigitalaxGarmentDesigner} from "../../generated/schema";
+import {NFT_DESIGNER_MAP} from "../constants";
+import { BigInt } from "@graphprotocol/graph-ts";
 
-export function loadOrCreateGarmentDesigner(garmentDesignerId: string): DigitalaxGarmentDesigner {
-    let garmentDesigner = DigitalaxGarmentDesigner.load(garmentDesignerId);
-    if (garmentDesigner == null) {
-        garmentDesigner = new DigitalaxGarmentDesigner(garmentDesignerId);
+export function loadOrCreateGarmentDesigner(tokenId: BigInt): DigitalaxGarmentDesigner {
+    let designerId = NFT_DESIGNER_MAP[tokenId.toI32()];
+    let garmentDesigner = DigitalaxGarmentDesigner.load(designerId);
+    //if (garmentDesigner == null) {
+        garmentDesigner = new DigitalaxGarmentDesigner(designerId);
         garmentDesigner.garments = new Array<string>();
         garmentDesigner.listings = new Array<string>();
-    }
+    //}
     garmentDesigner.save()
     return garmentDesigner as DigitalaxGarmentDesigner
 }
