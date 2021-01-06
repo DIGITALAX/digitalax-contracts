06ee127a5ca8a91d5ac288360a31873c6ea9dff7
Victor Wiebe <victorgypsy@gmail.com>
2021-01-05 21:57:49 -0400

    Remove unneeded code

--- smart-contracts/test/DigitalaxGarmentCollection.test.js
+++ smart-contracts/test/DigitalaxGarmentCollection.test.js
@@ -2,9 +2,7 @@ const {
   expectRevert,
   expectEvent,
   BN,
-  ether,
   constants,
-  balance
 } = require('@openzeppelin/test-helpers');
 
 const {expect} = require('chai');
@@ -13,18 +11,6 @@ const DigitalaxAccessControls = artifacts.require('DigitalaxAccessControls');
 const DigitalaxMaterials = artifacts.require('DigitalaxMaterials');
 const DigitalaxGarmentNFT = artifacts.require('DigitalaxGarmentNFT');
 const DigitalaxGarmentCollection = artifacts.require('DigitalaxGarmentCollection');
-const DigitalaxMarketplace = artifacts.require('DigitalaxMarketplaceMock');
-const MockERC20 = artifacts.require('MockERC20');
-const UniswapPairOracle_MONA_WETH = artifacts.require('UniswapPairOracle_MONA_WETH');
-const UniswapV2Router02 = artifacts.require('UniswapV2Router02');
-const UniswapV2Factory = artifacts.require('UniswapV2Factory');
-const UniswapV2Pair = artifacts.require('UniswapV2Pair');
-const WethToken = artifacts.require('WethToken');
-
-// 1,000 * 10 ** 18
-const ONE_THOUSAND_TOKENS = '1000000000000000000000';
-const TWO_HUNDRED_TOKENS = new BN('200000000000000000000');
-const TWENTY_TOKENS = new BN('20000000000000000000');
 
 contract('DigitalaxGarmentCollection', (accounts) => {
   const [admin, smartContract, platformFeeAddress, minter, owner, designer, tokenBuyer, newRecipient] = accounts;
@@ -37,14 +23,6 @@ contract('DigitalaxGarmentCollection', (accounts) => {
   beforeEach(async () => {
     this.accessControls = await DigitalaxAccessControls.new({from: admin});
     await this.accessControls.addMinterRole(minter, {from: admin});
-    await this.accessControls.addSmartContractRole(smartContract, {from: admin});
-
-    this.monaToken = this.token = await MockERC20.new(
-        'MONA',
-        'MONA',
-        ONE_THOUSAND_TOKENS,
-        {from: tokenBuyer}
-    );
 
     this.digitalaxMaterials = await DigitalaxMaterials.new(
       'DigitalaxMaterials',
@@ -59,53 +37,11 @@ contract('DigitalaxGarmentCollection', (accounts) => {
       {from: admin}
     );
 
-    this.factory = await UniswapV2Factory.new(
-      owner,
-      { from: owner }
-    );
-
-    this.weth = await WethToken.new(
-      { from: minter }
-    );
-
-    this.monaWETH = await UniswapV2Pair.at(
-        (await this.factory.createPair(this.monaToken.address, this.weth.address)).logs[0].args.pair
-    );
-    await this.weth.transfer(this.monaWETH.address, TWENTY_TOKENS, { from: minter });
-    await this.monaToken.transfer(this.monaWETH.address, TWO_HUNDRED_TOKENS, { from: tokenBuyer });
-    await this.monaWETH.mint(minter);
-
-    this.router02 = await UniswapV2Router02.new(
-      this.factory.address,
-      this.weth.address
-    );
-
-    this.oracle = await UniswapPairOracle_MONA_WETH.new(
-      this.factory.address,
-      this.monaToken.address,
-      this.weth.address
-    );
-
     this.garmentCollection = await DigitalaxGarmentCollection.new(
       this.accessControls.address,
       this.token.address
     );
     await this.accessControls.addMinterRole(this.garmentCollection.address, {from: admin});
-
-    this.marketplace = await DigitalaxMarketplace.new(
-      this.accessControls.address,
-      this.token.address,
-      this.garmentCollection.address,
-      this.oracle.address,
-      platformFeeAddress,
-      this.monaToken.address,
-      this.weth.address,
-      {from: admin}
-    );
-
-    this.monaToken.approve(this.marketplace.address, ONE_THOUSAND_TOKENS);
-
-    await this.accessControls.addSmartContractRole(this.marketplace.address, {from: admin});
   });
 
   describe('Contract deployment', () => {
@@ -141,10 +77,6 @@ contract('DigitalaxGarmentCollection', (accounts) => {
           collectionId: (new BN('0')),
         });
 
-        const garmentIds = await this.garmentCollection.getTokenIds(0);
-        for (let i = 0; i < garmentIds.length; i ++) {
-          await this.token.approve(this.marketplace.address, garmentIds[i], {from: minter});
-        }
         const owner = await this.token.ownerOf(TOKEN_ONE_ID);
         expect(owner).to.be.equal(minter);
       });
@@ -154,10 +86,6 @@ contract('DigitalaxGarmentCollection', (accounts) => {
   describe('burnCollection()', async () => {
     beforeEach(async () => {
       await this.garmentCollection.mintCollection(minter, randomTokenURI, designer, COLLECTION_SIZE, {from: minter});
-      const garmentIds = await this.garmentCollection.getTokenIds(0);
-      for (let i = 0; i < garmentIds.length; i ++) {
-        await this.token.approve(this.marketplace.address, garmentIds[i], {from: minter});
-      }
     });
 
     describe('validation', async () => {
@@ -185,10 +113,6 @@ contract('DigitalaxGarmentCollection', (accounts) => {
   describe('getters', async () => {
     beforeEach(async () => {
       await this.garmentCollection.mintCollection(minter, randomTokenURI, designer, COLLECTION_SIZE, {from: minter});
-      const garmentIds = await this.garmentCollection.getTokenIds(0);
-      for (let i = 0; i < garmentIds.length; i ++) {
-        await this.token.approve(this.marketplace.address, garmentIds[i], {from: minter});
-      }
     });
 
     describe('validation', async () => {
