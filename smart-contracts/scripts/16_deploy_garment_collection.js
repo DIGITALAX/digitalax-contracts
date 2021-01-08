aeca7ac5ffcfb82ee0cfdf821f61978afbc216bb
Chaindev <ikeda.okura@gmail.com>
2021-01-08 10:23:27 -0300

    collection smart contract update for minting with children

--- smart-contracts/scripts/16_deploy_garment_collection.js
+++ smart-contracts/scripts/16_deploy_garment_collection.js
@@ -4,15 +4,17 @@ async function main() {
     const deployerAddress = await deployer.getAddress();
     console.log('Deploying DigitalaxGarmentCollection with the account:', deployerAddress);
   
-    const {ACCESS_CONTROLS_ADDRESS, ERC721_GARMENT_ADDRESS} = process.env;
+    const {ACCESS_CONTROLS_ADDRESS, ERC721_GARMENT_ADDRESS, ERC1155_MATERIALS_ADDRESS} = process.env;
     console.log(`ACCESS_CONTROLS_ADDRESS found [${ACCESS_CONTROLS_ADDRESS}]`);
     console.log(`ERC721_GARMENT_ADDRESS found [${ERC721_GARMENT_ADDRESS}]`);
+    console.log(`ERC1155_MATERIALS_ADDRESS found [${ERC1155_MATERIALS_ADDRESS}]`);
   
   
     const DigitalaxGarmentCollection = await ethers.getContractFactory('DigitalaxGarmentCollection');
     const contract = await DigitalaxGarmentCollection.deploy(
       ACCESS_CONTROLS_ADDRESS,
       ERC721_GARMENT_ADDRESS,
+      ERC1155_MATERIALS_ADDRESS,
     );
   
     await contract.deployed();
