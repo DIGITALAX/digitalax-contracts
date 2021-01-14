6184db67dcdbd64e0a116bf171cd691522a95bb0
Chaindev <ikeda.okura@gmail.com>
2021-01-14 16:59:15 -0300

    Marketplace subgraph fixes for Global Stats and amountSold

--- subgraph/src/factory/DigitalaxMarketplaceGlobalStats.factory.ts
+++ subgraph/src/factory/DigitalaxMarketplaceGlobalStats.factory.ts
@@ -1,15 +1,15 @@
 import {DigitalaxMarketplaceGlobalStat} from "../../generated/schema";
 import {ZERO} from "../constants";
 
-export function loadOrCreateMarketplaceGlobalStats(): DigitalaxMarketplaceGlobalStat | null {
-    let globalStats = DigitalaxMarketplaceGlobalStat.load('DigitalaxGarmentNFTGlobalStats');
+export function loadOrCreateMarketplaceGlobalStats(): DigitalaxMarketplaceGlobalStat {
+    let globalStats = DigitalaxMarketplaceGlobalStat.load('0');
 
     if (globalStats == null) {
-        globalStats = new DigitalaxMarketplaceGlobalStat('DigitalaxMarkeplaceGlobalStats');
+        globalStats = new DigitalaxMarketplaceGlobalStat('0');
         globalStats.totalETHSalesValue = ZERO;
         globalStats.totalMonaSalesValue = ZERO;
-        globalStats.save();
     }
+    globalStats.save();
 
-    return globalStats;
+    return globalStats as DigitalaxMarketplaceGlobalStat;
 }
