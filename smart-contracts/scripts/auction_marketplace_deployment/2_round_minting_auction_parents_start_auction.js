fadbde0a14cac256c7af00e62c0166fda6d814fd
Victor Wiebe <victorgypsy@gmail.com>
2021-01-15 14:02:46 -0400

    Ready for main net

--- smart-contracts/scripts/auction_marketplace_deployment/2_round_minting_auction_parents_start_auction.js
+++ smart-contracts/scripts/auction_marketplace_deployment/2_round_minting_auction_parents_start_auction.js
@@ -41,22 +41,24 @@ async function main() {
 
     // OPTIONAL TODO use if needed
    // await accessControls.addSmartContractRole(AUCTION_ADDRESS);
-   //
+   // OPTIONAL TODO use if needed
    // const updateFee = await auction.updatePlatformFee('500');
    // await updateFee.wait();
 
   //// SETTINGS
   // fill in uris for the nfts
-  const restingreen_harajuku =  ['137']; // rest in green harajuku --> mainnet 80 TODO confirm, update for mainnet
-  const apis_mechanicus_transformation =  ['140']; // apis mechanicus --> mainnet 64 TODO confirm, update for mainnet
+  const restingreen_harajuku =  ['80']; // rest in green harajuku --> mainnet 80 TODO confirm
+  const apis_mechanicus_transformation =  ['64']; // apis mechanicus --> mainnet 64 TODO confirm
+  const stripes_fewcharm =  ['55']; // stripes, few charm --> mainnet 55 TODO confirm
+  const turnheartupsidedown_cosmicone =  ['66']; // turn heart updside down, cosmic one --> mainnet 66 TODO confirm
   const tokenIdAmounts = ['1'];
 
   const reservePrice = '0';
   const reservePrice2 = '0';
 
- // const startTime = '1606347000'; // 11/25/2020 @ 11:30pm (UTC) | 3:30pm pst November 25th only test
-    const mainnet_startTime = '1610740800'; // Jan 15, 8pm utc
-   const mainnet_endTime = '1611086400';   // Jan 19, 8pm utc
+//  const testStartTime = '1606347000'; // 11/25/2020 @ 11:30pm (UTC) | 3:30pm pst November 25th only test
+   const mainnet_startTime = '1610740800'; // Jan 15, 8pm utc TODO confirm
+   const mainnet_endTime = '1611086400';   // Jan 19, 8pm utc TODO confirm
 
     // Run 1 at a time in production, in case something drops
   const uris = [
@@ -79,6 +81,26 @@ async function main() {
           auctionStartTime: mainnet_startTime,
           auctionEndTime: mainnet_endTime,
           designer: ROUND_3_DESIGNERS.yekatarina,
+      },
+      {
+          // Auction 3 Exclusive
+          uri: require('../../../../nft-minting-scripts/auction-metadata/token-data/parents/Few Charm/hash.json').uri,
+          tokendIds: stripes_fewcharm,
+          tokenAmounts: tokenIdAmounts,
+          price: reservePrice,
+          auctionStartTime: mainnet_startTime,
+          auctionEndTime: mainnet_endTime,
+          designer: ROUND_3_DESIGNERS.lorena
+      },
+      {
+          // Auction 4 Exclusive
+          uri: require('../../../../nft-minting-scripts/auction-metadata/token-data/parents/Cosmic One/hash.json').uri,
+          tokendIds: turnheartupsidedown_cosmicone,
+          tokenAmounts: tokenIdAmounts,
+          price: reservePrice2,
+          auctionStartTime: mainnet_startTime,
+          auctionEndTime: mainnet_endTime,
+          designer: ROUND_3_DESIGNERS.nina,
       }
   ]
 
