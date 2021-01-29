2de234a90b44ce731687f5b05c9912c17dee580e
Victor Wiebe <victorgypsy@gmail.com>
2021-01-28 21:25:21 -0400

    Modify to use the cycles concept to create both pending and claimable rewards for mona revenue sharing

--- smart-contracts/contracts/staking/interfaces/IDigitalaxRewards.sol
+++ smart-contracts/contracts/staking/interfaces/IDigitalaxRewards.sol
@@ -5,6 +5,7 @@ pragma solidity 0.6.12;
 /// @dev an interface to interact with the Genesis MONA NFT that will 
 interface IDigitalaxRewards {
     function updateRewards(uint256 _poolId) external returns (bool);
-    function MonaRewards(uint256 _poolId, uint256 _from, uint256 _to) external view returns(uint256);
+    function MonaRevenueRewards(uint256 _poolId, uint256 _from, uint256 _to) external view returns(uint256);
+    function ETHRevenueRewards(uint256 _poolId, uint256 _from, uint256 _to) external view returns(uint256);
     function lastRewardsTime(uint256 _poolId) external view returns (uint256);
 }
