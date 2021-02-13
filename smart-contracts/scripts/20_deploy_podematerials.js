326d271f6ea5d420f6e7defb7bef08ccb5375cff
Chaindev <ikeda.okura@gmail.com>
2021-02-13 18:07:22 -0300

    PodeMaterials contract

---
+++ smart-contracts/scripts/20_deploy_podematerials.js
@@ -0,0 +1,59 @@
+const metadataList = [
+    require('../../../metadata/01/hash.json').uri,
+    require('../../../metadata/02/hash.json').uri,
+    require('../../../metadata/03/hash.json').uri,
+    require('../../../metadata/04/hash.json').uri,
+    require('../../../metadata/05/hash.json').uri,
+    require('../../../metadata/06/hash.json').uri,
+    require('../../../metadata/07/hash.json').uri,
+    require('../../../metadata/08/hash.json').uri,
+    require('../../../metadata/09/hash.json').uri,
+    require('../../../metadata/10/hash.json').uri,
+    require('../../../metadata/11/hash.json').uri,
+    require('../../../metadata/12/hash.json').uri, 
+    require('../../../metadata/13/hash.json').uri,
+]
+
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+  const deployerAddress = await deployer.getAddress();
+  console.log('Deploying DigitalaxPodeMaterials with the account:', deployerAddress);
+
+  const {
+    ACCESS_CONTROLS_ADDRESS,
+    PODE_NFT_ADDRESS,
+  } = process.env;
+
+  console.log(`Deploying DigitalaxPodeMaterials`, {
+    ACCESS_CONTROLS_ADDRESS,
+    PODE_NFT_ADDRESS,
+  });
+
+  const DigitalaxPodeMaterials = await ethers.getContractFactory('DigitalaxPodeMaterials');
+  const contract = await DigitalaxPodeMaterials.deploy(
+    'DigitalaxPodeMaterials',
+    'PODEM',
+    ACCESS_CONTROLS_ADDRESS,
+    PODE_NFT_ADDRESS,
+  );
+
+  await contract.deployed();
+
+  console.log('DigitalaxPodeMaterials deployed to:', contract.address);
+  
+  for (tokenUri of metadataList) {
+    const tx = await contract.addTokenURI(tokenUri);
+    await tx.wait();
+    console.log('---Metadata Added---');
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
