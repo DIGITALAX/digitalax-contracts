a773a51154001a8733d8a3ab64bd771d4d1aa9cc
Victor Wiebe <victorgypsy@gmail.com>
2021-04-01 18:13:27 -0400

    Compatible with abi encoder v2

--- smart-contracts/contracts/garment/DigitalaxGarmentNFTv2.sol
+++ smart-contracts/contracts/garment/DigitalaxGarmentNFTv2.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155Receiver.sol";
--- smart-contracts/contracts/tunnel/DigitalaxRootTunnel.sol
+++ smart-contracts/contracts/tunnel/DigitalaxRootTunnel.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import {BaseRootTunnel} from "./BaseRootTunnel.sol";
 import "../garment/DigitalaxGarmentNFT.sol";
