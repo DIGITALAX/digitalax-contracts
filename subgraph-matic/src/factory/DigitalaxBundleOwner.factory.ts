d0f174fc8f19fb3dc5e00ef4f6ccfe84a2b97f7a
Victor Wiebe <victorgypsy@gmail.com>
2021-05-06 18:01:28 -0400

    WIP Subgraph subscription changes

---
+++ subgraph-matic/src/factory/DigitalaxBundleOwner.factory.ts
@@ -0,0 +1,30 @@
+import {BigInt, ethereum, Address} from "@graphprotocol/graph-ts/index";
+import {ZERO} from "../constants";
+
+import {DigitalaxBundleOwner} from "../../generated/schema";
+import {DFBundle as DFBundleContract} from "../../generated/DFBundle/DFBundle";
+
+export function loadOrCreateDigitalaxBundleOwner(
+    event: ethereum.Event,
+    parentTokenOwner: Address,
+    childTokenId: BigInt
+): DigitalaxBundleOwner {
+
+    let childContract = DFBundleContract.bind(event.address);
+
+    // {parent-token-id-holder}-{child-token-id}
+    let childId = parentTokenOwner.toHexString() + '-' + childTokenId.toString();
+
+    let materialOwner = DigitalaxBundleOwner.load(childId);
+    if (materialOwner == null) {
+        materialOwner = new DigitalaxBundleOwner(childId);
+        materialOwner.childId = childTokenId;
+        materialOwner.owner = parentTokenOwner;
+        materialOwner.contract = event.address;
+        materialOwner.tokenUri = childContract.uri(childTokenId);
+        materialOwner.amount = ZERO
+    }
+    materialOwner.save();
+
+    return materialOwner as DigitalaxBundleOwner;
+}
