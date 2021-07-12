f2d3bfa416db4d9ecfa55428804966a6d7715883
jupiteruwdev <harutoinoue8@gmail.com>
2021-07-12 15:52:48 -0500

    typo issues fixed.

--- subgraph-matic/src/DripOracle.mapping.ts
+++ subgraph-matic/src/DripOracle.mapping.ts
@@ -3,12 +3,12 @@ import { PayableTokenReport } from "../generated/schema";
 
 export function handleProviderReportsPushed(event: ProviderReportsPushed): void {
     const tokens = event.params.payableTokens;
-    const paylaods = event.params.payloads;
+    const payloads = event.params.payloads;
     const timestamp = event.params.timestamp;
 
     for (let i = 0; i < tokens.length; i += 1) {
         const tokenReport = new PayableTokenReport(tokens[i].toHexString());
-        tokenReport.payload = paylaods[i];
+        tokenReport.payload = payloads[i];
         tokenReport.timestamp = timestamp;
         tokenReport.save();
     }
