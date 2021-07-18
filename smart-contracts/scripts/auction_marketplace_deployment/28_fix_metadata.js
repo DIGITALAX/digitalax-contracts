c126f38b48bd7fcb2a52878d1e9f328cddb92e04
onigiri <victor@digitalax.xyz>
2021-07-18 15:03:52 -0400

    EMMA read me

--- smart-contracts/scripts/auction_marketplace_deployment/28_fix_metadata.js
+++ smart-contracts/scripts/auction_marketplace_deployment/28_fix_metadata.js
@@ -22,9 +22,27 @@ async function main() {
       deployer
   );
 
-  const firstTokenId = 111232;
+  // EMMA FILL ME OUT BELOW
+  // THIS SCRIPT IS READY TO RUN FOR RARI CAP 1
+  // AFTER 1st RUNNING IT, for 2nd run, UPDATE LINE 43 with the next "first" token id
+  // And the line below , line 45 with the token uri
+  // You can double check the script and the excel sheet to make sure there is accuracy.
+
+  //     112832	https://digitalax.mypinata.cloud/ipfs/QmXW1KznmKSq2pT5buZ5paoB53dTJcbR3j7zD1gQxEicgk
+  //     113032	https://digitalax.mypinata.cloud/ipfs/QmSrRZJe5EmSokixYqYuNGDANKix2mp7LmZTEuUmHRJfbS
+  //     113232	https://digitalax.mypinata.cloud/ipfs/QmUtk91RFne78TmGCLYGvw5mdgfM5bYsxo22N6Wi8VsQ6h
+  //     113432	https://digitalax.mypinata.cloud/ipfs/QmPmgVgMmrduUM6UcdnrmixzVku77huECyo27ED1QgAn3n
+  //     113632	https://digitalax.mypinata.cloud/ipfs/QmNqYoJ6HziXiK99v5irw9v6euMd8iZoRr2zrsMBDjqZTk
+  //     114552	https://digitalax.mypinata.cloud/ipfs/QmbmAoj8bczeGhw4QcYtQrw4tDuMi13ZiCEX2FV11sHbtY
+  //     114752	https://digitalax.mypinata.cloud/ipfs/QmZhmCBXTJeKHhEVL34qZWuoS7JFcFvMR76Dp3WS1oSJ97
+  //     114952	https://digitalax.mypinata.cloud/ipfs/QmUyoSufLttjegdfMdFWrHrVfYc7iZ5JJsGYaAY4hvXb1k
+  //     115712	https://digitalax.mypinata.cloud/ipfs/QmbFe63jfCPi6mLPPHnvb1qJkV6vs4aX9XFBc8N9NZ6Vk8
+  //     115912	https://digitalax.mypinata.cloud/ipfs/QmRet5pP9i3C71fMrm8Zaii2hnutYRiRRLfiPFhnfCu1HS
+  //     116112	https://digitalax.mypinata.cloud/ipfs/QmSHq7fxeiiQS7g4p1Py1QG1HA1u24Jv319iBCqB5V1PDM
+
+  const firstTokenId = 112632;
   const updatedURI_fill_me =
-      'https://digitalax.mypinata.cloud/ipfs/QmVPQDF43gb1kd56xMzBKS99nXpPED5pouu5hJW85xM9YS';
+      'https://digitalax.mypinata.cloud/ipfs/QmUPMdGKDkipEfasF9t4P9UxdkXCs7gS8ByQZW6t951EXA';
 
   const newUriArray = new Array(QUANTITY_NFT_SINGLE_BATCH_TX).fill(updatedURI_fill_me);
   const lastTokenId = firstTokenId + 199; // 200 total quantity
