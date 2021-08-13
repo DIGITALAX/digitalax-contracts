c8a248c6bb741294218e3c1c679cb8ab23fc0cfd
CameronS29 <c.sato.dev@gmail.com>
2021-08-13 17:17:04 -0400

    guildwhitelistednftstaking handlers updated.

--- subgraph-matic/src/GuildWhitelistedNFTStaking.mapping.ts
+++ subgraph-matic/src/GuildWhitelistedNFTStaking.mapping.ts
@@ -1,3 +1,5 @@
+import { Bytes, ipfs, json, JSONValue, JSONValueKind } from '@graphprotocol/graph-ts';
+import { ERC721 } from '../generated/GuildWhitelistedNFTStaking/ERC721';
 import {
     Staked,
     Unstaked,
@@ -7,6 +9,7 @@ import {
 } from "../generated/GuildWhitelistedNFTStaking/GuildWhitelistedNFTStaking";
 
 import {
+    GuildWhitelistedNFT,
     GuildWhitelistedNFTStaker,
 } from "../generated/schema";
 import {ZERO} from "./constants";
@@ -26,11 +29,41 @@ export function handleRewardPaid(event: RewardPaid): void {
 }
 
 export function handleStaked(event: Staked): void {
-    let contract = GuildWhitelistedNFTStakingContract.bind(event.address);
+    let contract = ERC721.bind(event.params.whitelistedNFT);
+    let tokenUri = contract.tokenURI(event.params.tokenId);
     let owner = event.params.owner.toHexString()
     let token = event.params.tokenId.toString();
     let whitelistedNft = event.params.whitelistedNFT.toHexString();
     let garmentId = whitelistedNft.concat('-').concat(token);
+    let garment = new GuildWhitelistedNFT(garmentId);
+    garment.owner = owner;
+    garment.tokenUri = tokenUri;
+    garment.tokenAddress = whitelistedNft;
+    if (tokenUri) {
+        if (tokenUri.includes('ipfs/')) {
+            let tokenHash = tokenUri.split('ipfs/')[1];
+            let tokenBytes = ipfs.cat(tokenHash);
+            if (tokenBytes) {
+                let data = json.try_fromBytes(tokenBytes as Bytes);
+                if (data.isOk) {
+                    if (data.value.kind === JSONValueKind.OBJECT) {
+                        let res = data.value.toObject();
+                        if (res.get('animation_url').kind === JSONValueKind.STRING) {
+                            garment.animation = res.get('animation_url').toString();
+                        }
+                        if (res.get('description').kind === JSONValueKind.STRING) {
+                            garment.description = res.get('description').toString();
+                        }
+                        if (res.get('name').kind === JSONValueKind.STRING) {
+                            garment.name = res.get('name').toString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    garment.save();
+
     let staker = GuildWhitelistedNFTStaker.load(owner);
     if (staker == null) {
         staker = new GuildWhitelistedNFTStaker(owner);
