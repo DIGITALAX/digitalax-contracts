6d12dc4066e75e224be3d64edc186567047281bc
onigiri-x <victor@digitalax.xyz>
2021-09-07 00:31:26 -0400

    Weight keeps its own time again

--- smart-contracts/contracts/mock/GuildNFTStakingWeightV2StorageMock.sol
+++ smart-contracts/contracts/mock/GuildNFTStakingWeightV2StorageMock.sol
@@ -6,13 +6,4 @@ pragma experimental ABIEncoderV2;
 import "../staking/GuildNFTStakingWeightV2Storage.sol";
 
 contract GuildNFTStakingWeightV2StorageMock is GuildNFTStakingWeightV2Storage {
-    uint256 public nowOverride;
-
-    function setNowOverride(uint256 _now) external {
-        nowOverride = _now;
-    }
-
-    function _getNow() internal override view returns (uint256) {
-        return nowOverride;
-    }
 }
