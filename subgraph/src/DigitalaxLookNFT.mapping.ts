7b67dccb7679fb7ac05358c63de89a41644aea97
onigiri-x <victor@digitalax.xyz>
2021-10-08 18:17:35 -0400

    Fix for look

--- subgraph/src/DigitalaxLookNFT.mapping.ts
+++ subgraph/src/DigitalaxLookNFT.mapping.ts
@@ -4,7 +4,7 @@ import {
 } from "../generated/DigitalaxLookNFT/DigitalaxLookNFT";
 import { ZERO_ADDRESS } from "./constants";
 import { DigitalaxLookNFT } from "../generated/schema";
-import { store } from "@graphprotocol/graph-ts";
+import { store, BigInt, log} from "@graphprotocol/graph-ts";
 
 export function handleTransfer(event: Transfer): void {
   let contract = DigitalaxLookNFTContract.bind(event.address);
@@ -30,6 +30,32 @@ export function handleTransfer(event: Transfer): void {
     digitalaxLookNft.tokenUri = contract.tokenURI(event.params.tokenId);
     digitalaxLookNft.color = contract.getColour(event.params.tokenId);
     digitalaxLookNft.save();
+    // Just for fixing the case where tokens were claimed before randomness drawn
+   if( event.block.timestamp > BigInt.fromI32(1633278600) && event.block.timestamp < BigInt.fromI32(1633691504)) // this basically just will be called for 1 transaction that happened 4 days ago.
+     {
+       log.info("Resyncing @ timestamp", [event.block.timestamp.toString()]);
+      for(let i = 1; i< 3001; i++){
+        const j = BigInt.fromI32(i);
+        let owner = contract.try_ownerOf(j);
+        if (!owner.reverted) {
+        let digitalaxLookNftUpdate = DigitalaxLookNFT.load(j.toString());
+          digitalaxLookNftUpdate.name = contract.name();
+          digitalaxLookNftUpdate.shape = contract.getShape(j);
+          digitalaxLookNftUpdate.pattern = contract.getPattern(j);
+          digitalaxLookNftUpdate.background = contract.getBackgroundColour(
+              j
+          );
+          digitalaxLookNftUpdate.texture = contract.getTexture(j);
+          digitalaxLookNftUpdate.flare = contract.getFlare(j);
+          digitalaxLookNftUpdate.form = contract.getForm(j);
+          digitalaxLookNftUpdate.line = contract.getLine(j);
+          digitalaxLookNftUpdate.element = contract.getElement(j);
+          digitalaxLookNftUpdate.tokenUri = contract.tokenURI(j);
+          digitalaxLookNftUpdate.color = contract.getColour(j);
+          digitalaxLookNftUpdate.save();
+        }
+      }
+    }
   } else if (event.params.to.equals(ZERO_ADDRESS)) {
     store.remove("DigitalaxLookNFT", event.params.tokenId.toString());
   } else {
