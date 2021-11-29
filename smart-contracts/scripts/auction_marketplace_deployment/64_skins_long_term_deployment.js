316eb3e8be1fddc6c1c6d88c375c942fe66e0247
onigiri-x <victor@digitalax.xyz>
2021-11-29 10:46:20 -0400

    working on genesis nft as copy of pode and the staking contract

--- smart-contracts/scripts/auction_marketplace_deployment/64_skins_long_term_deployment.js
+++ smart-contracts/scripts/auction_marketplace_deployment/64_skins_long_term_deployment.js
@@ -12,23 +12,11 @@ async function main() {
   const deployerAddress = await deployer.getAddress();
     console.log('Deploying the long term staking contracts');
 
-    // Some constants setup
-    // const accessControlsAddress = "0xf7580d46080E1ce832aC44cF7224b906D44110B4";
-    // const trustedForwarderAddress = "0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b";
-
     const accessControlsAddress = "0xbe5c84e6b036cb41a7a6b5008b9427a5f4f1c9f5";
     const trustedForwarderAddress = "0x86C80a8aa58e0A4fa09A69624c31Ab2a6CAD56b8";
-
-    // // Deploy oracle for deco (rewards token) price
-    // const DecoOracleFactory = await ethers.getContractFactory("DecoOracle");
-    // const decoOracle = await DecoOracleFactory.deploy( '315400000', '120', '1',  accessControlsAddress);
-
-    // const monaTokenAddress = '0xefd3d060ddcfed7903806503440db1089031af3a';
-    // const decoTokenAddress = '0xBCD79f68a91500a2Cc7C29e3AaA80046cDa29833';
-
     const monaTokenAddress = '0x6968105460f67c3BF751bE7C15f92F5286Fd0CE5';
     const skinsNFTAddress = '0x7B2a989c4D1AD1B79a84CE2EB79dA5D8d9C2b7a7';
-    const oracleAddress = '0xeE0b5F24d4b2cF80E3E54e9e5Ff8aCb9AFf1F8A4'; // todo check
+    const oracleAddress = '0xee0b5f24d4b2cf80e3e54e9e5ff8acb9aff1f8a4';
 
     const DigitalaxNFTStakingFactory = await ethers.getContractFactory("DigitalaxNFTStaking");
     const nftStakingDeploy = await upgrades.deployProxy(DigitalaxNFTStakingFactory,
@@ -61,12 +49,8 @@ async function main() {
     const updateRewardContract = await nftStaking.setRewardsContract(instanceStakingRewards.address);
     await updateRewardContract.wait();
 
-    // console.log('add extra tokens');
-    // const extraTokens = await instanceStakingRewards.addRewardTokens([decoTokenAddress]);
-    // await extraTokens.wait();
-
     console.log('start time');
-    const startT = await instanceStakingRewards.setStartTime(1635254591); // TODO double check start time sets correctly
+    const startT = await instanceStakingRewards.setStartTime(1637776800); // TODO double check start time sets correctly
     await startT.wait();
 
     console.log(`The rewards: ${instanceStakingRewards.address} `);
