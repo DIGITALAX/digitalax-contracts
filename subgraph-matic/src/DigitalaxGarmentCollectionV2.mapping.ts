35bb576f0cc6deb076592a0c9ba0816602ee27d6
onigiri-x <victor@digitalax.xyz>
2022-02-11 09:14:38 -0400

    Fixes and staking

--- subgraph-matic/src/DigitalaxGarmentCollectionV2.mapping.ts
+++ subgraph-matic/src/DigitalaxGarmentCollectionV2.mapping.ts
@@ -1,4 +1,4 @@
-import { log, ipfs, JSONValue, Value } from "@graphprotocol/graph-ts/index";
+import { log, ipfs, JSONValue, Value, store } from "@graphprotocol/graph-ts/index";
 import {
     MintGarmentCollection,
     BurnGarmentCollection,
@@ -12,37 +12,52 @@ import {
 import {ZERO} from "./constants";
 
 export function handleGarmentCollectionMinted(event: MintGarmentCollection): void {
-    let contract = DigitalaxGarmentCollectionV2Contract.bind(event.address);
-    let collectionData = contract.getCollection(event.params.collectionId);
-    let collection = DigitalaxGarmentV2Collection.load(event.params.collectionId.toString());
-    if(!collection){
-        collection = new DigitalaxGarmentV2Collection(event.params.collectionId.toString());
-        collection.garmentAuctionID = event.params.auctionTokenId;
-        collection.rarity = event.params.rarity;
-        collection.valueSold = ZERO;
-        let mintedGarments = new Array<string>();
-        for(let i = 0; i < collectionData.value1.toI32(); i++) {
-            let garmentToken = DigitalaxGarmentV2.load(collectionData.value0[i].toString());
-            mintedGarments.push(garmentToken.id);
+        let contract = DigitalaxGarmentCollectionV2Contract.bind(event.address);
+        let collectionData = contract.getCollection(event.params.collectionId);
+        let collection = DigitalaxGarmentV2Collection.load(event.params.collectionId.toString());
+        if (!collection) {
+            collection = new DigitalaxGarmentV2Collection(event.params.collectionId.toString());
+            collection.garmentAuctionID = event.params.auctionTokenId;
+            collection.rarity = event.params.rarity;
+            collection.valueSold = ZERO;
+            let mintedGarments = new Array<string>();
+            if(collectionData) {
+                for (let i = 0; i < collectionData.value1.toI32(); i++) {
+                    if (collectionData.value0) {
+                         if(collectionData.value0[i]) {
+                            let garmentToken = DigitalaxGarmentV2.load(collectionData.value0[i].toString());
+                            if (garmentToken) {
+                                mintedGarments.push(garmentToken.id);
+                            }
+                        }
+                    }
+                }
+            }
+            collection.garments = mintedGarments;
+        } else {
+            // This is the case for "mint more nfts on collection"
+            let updatedGarments = collection.garments;
+            if(collectionData) {
+                for (let i = 0; i < collectionData.value1.toI32(); i++) {
+                    if (collectionData.value0) {
+                        if(collectionData.value0[i]) {
+                            let garmentToken = DigitalaxGarmentV2.load(collectionData.value0[i].toString());
+                            if (garmentToken) {
+                                updatedGarments.push(garmentToken.id);
+                            }
+                        }
+                    }
+                }
+            }
+            collection.garments = updatedGarments;
         }
-        collection.garments = mintedGarments;
-    } else {
-        // This is the case for "mint more nfts on collection"
-        let updatedGarments = collection.garments;
-        for (let i = 0; i < collectionData.value1.toI32(); i++) {
-            let garmentToken = DigitalaxGarmentV2.load(collectionData.value0[i].toString());
-            updatedGarments.push(garmentToken.id);
-        }
-        collection.garments = updatedGarments;
-    }
-    collection.save();
+        collection.save();
 }
 
 
 export function handleGarmentCollectionBurned(event: BurnGarmentCollection): void {
-    let collection = DigitalaxGarmentV2Collection.load(event.params.collectionId.toString());
-    collection.garments = null;
-    collection.garmentAuctionID = null;
-    collection.rarity = null;
-    collection.save();
+        let collection = DigitalaxGarmentV2Collection.load(event.params.collectionId.toString());
+        if (!collection) {
+            store.remove("DigitalaxGarmentV2Collection", event.params.collectionId.toString());
+        }
 }
