9a6a72cb8d22e170922309a52acb93255a3c5903
onigiri-x <victor@digitalax.xyz>
2022-04-07 09:30:52 -0400

    TODOS

--- smart-contracts/scripts/auction_marketplace_deployment/90_nouns_deploy.js
+++ smart-contracts/scripts/auction_marketplace_deployment/90_nouns_deploy.js
@@ -19,32 +19,25 @@ async function main() {
   const nounsTokenAddress = nounsToken.address; //'0xEA11d04DEc0B6aec34fF49f1811F86417b0d952C';
   console.log(`nounsToken at: ${nounsTokenAddress} `);
 
-  // TODO
+  // TODO 1 Fill out daily uri
   const tx1 = await nounsToken.setDailyUris([1], Array(1).fill("https://digitalax.mypinata.cloud/ipfs/QmbF8aj3BoaLFQTxgYJa71TnZcG1mHSp7x6fmzSFXESBq9"));
   await tx1.wait();
-  // TODO
+  // TODO 2 Fill our next dao uri nft metadata
   const tx2 = await nounsToken.setNextDaoNFTUri("https://digitalax.mypinata.cloud/ipfs/Qmanarwy9GpHGwGiRzjTJLiumfL4o1xuhg4MgnbznM5pgk");
   await tx2.wait();
 
   const contractFactory2 = await ethers.getContractFactory("NounsAuctionHouse");
-  //      .addOptionalParam('auctionTimeBuffer', 'The auction time buffer (seconds)', 30, types.int) // Default: 30 seconds
-  // .addOptionalParam('auctionReservePrice', 'The auction reserve price (wei)', 1, types.int) // Default: 1 wei
-  // .addOptionalParam(
-  //   'auctionMinIncrementBidPercentage',
-  //   'The auction min increment bid percentage (out of 100)', // Default: 5%
-  //   5,
-  //   types.int,
-  // )
-  // .addOptionalParam('auctionDuration', 'The auction duration (seconds)', 60 * 2, types.int) // Default: 2 minutes
 
-// WILL BE 0.1 ETH not 1 WEI
-  // TODO
+  // TODO 3 Replace 10000000000000000000 with the reserve price in wei
   const nounsTokenAuction = await upgrades.deployProxy(contractFactory2, [nounsTokenAddress, "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", 30, '10000000000000000000', 5, 0]);
   await nounsTokenAuction.deployed();
 
+  // TODO 4 Add an erc20 token below that can be used to pay for the token
   const tx3 = await nounsTokenAuction.updateMonaToken("0x91Dbd2951310e887940C29D9828556086a52c54A");
   await tx3.wait();
-  const tx4 = await nounsTokenAuction.updateOraclePrice('1000000000000000000'); // TODO
+
+  // TODO 5 update the conversion rate below from erc20 token to ETH equivalent
+  const tx4 = await nounsTokenAuction.updateOraclePrice('1000000000000000000');
   await tx4.wait();
   // const tx5 = await nounsTokenAuction.toggleFreezeETHBid();
   // await tx5.wait();
@@ -52,29 +45,33 @@ async function main() {
   await tx6.wait();
 
 
-  // const date = (Date.now() / 1000)
-  // console.log('it is currently');
-  // const dateFuture = 1648756800;
-// TODO
+
+  // TODO 6 Update in how much time in seconds will the first auction complete
   const tx8 = await nounsTokenAuction.updateDuration('104800');
   await tx8.wait();
   const tx9 = await nounsTokenAuction.settleCurrentAndCreateNewAuction();
   await tx9.wait();
 
+  // TODO 7 Update how long the subsequent auctions will be
   const txDurationFix = await nounsTokenAuction.updateDuration('86400');
   await txDurationFix.wait();
 
+  // TODO 8 Set the owner of the nouns token auction
   const tx10 = await nounsTokenAuction.transferOwnership('0x713c95BC55d5FD1a7d54C21CF980406cc973f27b');
   await tx10.wait();
 
    const tx7 = await nounsToken.setMinter(nounsTokenAuction.address);
   await tx7.wait();
 
+  // TODO 9 Set the start time for when you want the cycles to start
   const startTx = await nounsToken.setStartTime('1648756800');
   await startTx.wait();
+
+  // TODO 10 set the nounders dao ownership
   const tx11 = await nounsToken.setNoundersDAO('0x713c95BC55d5FD1a7d54C21CF980406cc973f27b');
   await tx11.wait();
 
+  // TODO 11 Set the token owner and admin
   const tx12 = await nounsToken.transferOwnership('0x713c95BC55d5FD1a7d54C21CF980406cc973f27b');
   await tx12.wait();
 
