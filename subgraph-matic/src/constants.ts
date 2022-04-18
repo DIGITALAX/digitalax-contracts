842c8c944ec2d40799f2b8b16bd607ca687fccc1
onigiri-x <victor@digitalax.xyz>
2022-04-18 15:18:05 -0400

    Change constant

--- subgraph-matic/src/constants.ts
+++ subgraph-matic/src/constants.ts
@@ -4,7 +4,7 @@ export const ZERO_ADDRESS = Address.fromString("0x000000000000000000000000000000
 export const ZERO_BIG_DECIMAL = BigDecimal.fromString("0")
 export const ZERO = BigInt.fromI32(0)
 export const ONE = BigInt.fromI32(1)
-export const ETH_IN_WEI = BigInt.fromString('1000000000000000000')
+export const ETH_IN_WEI = BigInt.fromI32(1).times(BigInt.fromI32(10).pow(18))
 export const MAX_UINT_256 = BigInt.fromUnsignedBytes(Bytes.fromHexString("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff") as Bytes)
 
 export const ONE_ETH = new BigDecimal(BigInt.fromI32(1).times(BigInt.fromI32(10).pow(18)))
